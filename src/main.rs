e9c4c1a3f5c67fe98e10b300b33df47d9ff4fd1f
Mayur Satardekar <mayursatardekar10@gmail.com>
2025-09-02 09:02:48 +0530

    Errors with expect and unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+// use std::fs::File;
+// use std::io::ErrorKind;
+
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main(){
-    let greeting_file = File::open("hello.txt");
+    // let greeting_file = File::open("hello.txt");
 
-    let _greeting_check = match greeting_file {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fs) => fs,
-                Err(e) => panic!("Failed to create file {e:?}"),                
-            },
-            _ => panic!("Failed to read the file"),
-        }       
-    };
+    // let _greeting_check = match greeting_file {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fs) => fs,
+    //             Err(e) => panic!("Failed to create file {e:?}"),                
+    //         },
+    //         _ => panic!("Failed to read the file"),
+    //     }       
+    // };
+    //using unwrap, if ok gives result or else it will panic
+    // let greeting_file = File::open("hello.txt").unwrap();
+
+    //using expect, if ok gives result or else it will panic but will give out the error message that we want.
+    let _greeting_file = File::open("hello.txt").expect("No such file exists, please check the file name");
 }
